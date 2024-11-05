d9fc0ea5ee6106a55144eb92ed12b8b5f3a01a62
Павел Сергеевич <p.bitiakov.official@gmail.com>
2024-11-05 16:25:04 +0300

    23.1: замена имени переменной

--- catalog/views.py
+++ catalog/views.py
@@ -12,7 +12,7 @@ class ProductListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context_data = super().get_context_data(*args, **kwargs)
         for product in context_data['product_list']:
-            active_version = Version.objects.filter(product=product, version_sign=True)
+            active_version = Version.objects.filter(product=product, is_current=True)
             if active_version:
                 product.active_version = active_version.last().version_name
             else:
