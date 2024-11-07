b177bb8ccea1aab9aa07b7e51e9b956fa623e4d4
Павел Сергеевич <p.bitiakov.official@gmail.com>
2024-11-07 16:42:31 +0300

    23.1: Допиливание кода

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.forms import inlineformset_factory
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from catalog.forms import ProductForm, VersionForm, ProductModeratorForm
+from catalog.mixins import CustomLoginRequiredMixin
 from catalog.models import Product, Version
 
 
@@ -70,13 +72,66 @@ class ProductCreateView(CreateView, LoginRequiredMixin):
         raise PermissionDenied
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(CustomLoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
 
-    def get_success_url(self):
-        return reverse("catalog:product_details", args=[self.kwargs.get('pk')])
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["title"] = "Редактирование товара"
+        VersionFormset = inlineformset_factory(
+            Product, Version, form=VersionForm, extra=1, can_delete=True
+        )
+        if self.request.method == "POST":
+            context_data["formset"] = VersionFormset(
+                self.request.POST, instance=self.object
+            )
+        else:
+            context_data["formset"] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context["formset"]
+        if form.is_valid() and formset.is_valid():
+            versions = [
+                form for form in formset if form.cleaned_data.get("is_active", False)
+            ]
+            if len(versions) > 1:
+                form.add_error(
+                    None, "У продукта не может быть более одной активной версии."
+                )
+                return self.form_invalid(form)
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        return self.form_invalid(form)
+
+    def get_form_class(self):
+        required_perms = [
+            "catalog.can_unpublish_product",
+            "catalog.can_change_description",
+            "catalog.can_change_category",
+        ]
+        user = self.request.user
+        if user == self.object.owner or user.is_superuser:
+            return ProductForm
+        if user.has_perms(required_perms):
+            return ProductModeratorForm
+        raise PermissionDenied
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if (
+            self.request.user == self.object.owner
+            or self.request.user.groups.filter(name="moderator").exists()
+            or self.request.user.is_superuser
+        ):
+            return self.object
+
+        raise PermissionDenied
 
 class ProductDeleteView(DeleteView):
     model = Product
