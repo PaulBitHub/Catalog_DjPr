64c7bd9816592dc2d84d95da69baa1cafa191d65
Павел Сергеевич <p.bitiakov.official@gmail.com>
2024-11-08 08:39:58 +0300

    23.1: Переписан класс создания продукта + коррекция условий отображения кнопок

--- catalog/views.py
+++ catalog/views.py
@@ -35,41 +35,32 @@ class ProductDetailView(DetailView):
         self.object.save()
         return self.object
 
-class ProductCreateView(CreateView, LoginRequiredMixin):
+class ProductCreateView(CustomLoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["title"] = "Создание товара"
+        VersionFormset = inlineformset_factory(
+            Product, Version, form=VersionForm, extra=1
+        )
+        if self.request.method == "POST":
+            context_data["formset"] = VersionFormset(self.request.POST)
+        else:
+            context_data["formset"] = VersionFormset()
+        return context_data
 
     def form_valid(self, form):
-        product = form.save()
-        user = self.request.user
-        product.owner = user
-        product.save()
+        form.instance.owner = self.request.user
+        context = self.get_context_data()
+        formset = context["formset"]
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
         return super().form_valid(form)
-
-    def get_form_class(self):
-        required_perms = [
-            "catalog.can_unpublish_product",
-            "catalog.can_change_description",
-            "catalog.can_change_category",
-        ]
-        user = self.request.user
-        if user == self.object.owner or user.is_superuser:
-            return ProductForm
-        if user.has_perms(required_perms):
-            return ProductModeratorForm
-        raise PermissionDenied
-
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        if (
-                self.request.user == self.object.owner
-                or self.request.user.groups.filter(name="moderator").exists()
-                or self.request.user.is_superuser
-        ):
-            return self.object
-
-        raise PermissionDenied
 
 
 class ProductUpdateView(CustomLoginRequiredMixin, UpdateView):
