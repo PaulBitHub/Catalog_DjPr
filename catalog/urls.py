a1eb8445fb25e44ac9851e059eb78566ded44115
PaulBitHub <p.bitiakov.official@gmail.com>
2024-11-19 14:59:36 +0300

    23.2: закеширован метод, добавлен Редис

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
     VersionCreateView, VersionUpdateView, VersionDeleteView, VersionDetailView, VersionListView
@@ -7,7 +9,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
-    path('product/<int:pk>', ProductDetailView.as_view(), name="product_details"),
+    path('product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name="product_details"),
     path("create/", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
